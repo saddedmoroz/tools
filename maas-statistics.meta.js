95f302742564955382ea25d1e816e6e0baca0ffe
saddedmoroz <98951088+saddedmoroz@users.noreply.github.com>
2024-06-27 17:41:35 +0200

    Delete maas-statistics.meta.js

--- maas-statistics.meta.js
+++
@@ -1,7 +0,0 @@
-// ==UserScript==
-// @name			Maas статистика
-// @version			1.5
-// @description		Отображает статистику работы
-// @author			t.me/yreawesome
-// @match			https://moderation.avito.ru/line/*
-// ==/UserScript==
