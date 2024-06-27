38d7e462ca4b76bdb1b57f4725e506391b7b026e
saddedmoroz <98951088+saddedmoroz@users.noreply.github.com>
2024-06-27 17:26:37 +0200

    Create maas-statistics.meta.js

---
+++ maas-statistics.meta.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name			Maas статистика
+// @version			1.4
+// @description		Отображает статистику работы
+// @author			t.me/yreawesome
+// @match			https://moderation.avito.ru/line/*
+// ==/UserScript==
