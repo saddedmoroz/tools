5a099ce1c3f969d767549d7b6c613ca4e91dc3f2
saddedmoroz <k.vvv333@ya.ru>
2025-03-29 00:20:52 +0200

    fix

--- maas-auth-by-key.js
+++ maas-auth-by-key.js
@@ -1,6 +1,3 @@
 // ==UserScript==
-// @name			Maas: авторизация по нажатию клавиши
 // @version			1.0
-// @updateURL		https://raw.githubusercontent.com/saddedmoroz/tools/refs/heads/master/maas-auth-by-key.js
-// @downloadURL		https://raw.githubusercontent.com/saddedmoroz/tools/refs/heads/master/maas-auth-by-key.js
 // ==/UserScript==
